ef5b6027f6ec833f0a9e27ad99912d8d99938b9c
Deejayayay <olad@spu.edu>
2023-10-20 11:06:36 -0700

    uhhh

--- HTTP_LibraryLab/coreHttp.js
+++ HTTP_LibraryLab/coreHttp.js
@@ -1,26 +1,46 @@
+const url = "https://jsonplaceholder.typicode.com/posts"
 class Core {
-    // constructor(URL) {
-    //     this.URL = URL;
-    // }
-
+    //constructor for the url 
+        //currently hardcoded but need to make it dynamic later
+    constructor(url) {
+        this.url = url;
+    }
+    
     async Get(url) { 
-        //Fix Request Options
-        const requestOptions = {
-            method: "GET",
-            //has to be the correct content type 'application/json'
-            headers: {"Content-Type": 'application/json'},
-            //use stringify in post 
-            //body: JSON.stringify(url)
-        }
-        const response = await fetch(url , requestOptions);
-        console.log(response);
-        if(response.ok) {
-            const posts = await response.json();
-            return posts;
-        } else {
-            const err = response.status();
-            return err;
-        }
+        fetch(url)
+            .then(response => {
+                if(response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('failed to fetch JSON');
+                }
+            })
+
+            .then(data => {
+                console.log(data);
+            })
+            .catch(error => {
+                console.error('Error: ', error);
+            });
+
+
+        // //Fix Request Options
+        // const requestOptions = {
+        //     method: "GET",
+        //     //has to be the correct content type 'application/json'
+        //     headers: {"Content-Type": 'application/json'},
+        //     //use stringify in post 
+        //     //body: JSON.stringify(url)
+        // }
+        // const response = await fetch(url , requestOptions);
+        // console.log(response);
+        // if(response.ok) {
+        //     const posts = await response.json();
+        //     return posts;
+        // } else {
+        //     const err = response.status();
+        //     return err;
+        // }
     }
 
     //post
--- HTTP_LibraryLab/scripts.js
+++ HTTP_LibraryLab/scripts.js
@@ -1,5 +1,16 @@
 let http = new Core;
 
 function processGet(){
+    data = Core.get;
+    let output; 
     
+    const posts = JSON.parse(data);
+    output = "<ul style=\"list-style:none\">";
+    for(let post of posts){
+        output += `<li>ID: ${post.id} \n Title: ${post.title} \n Body: ${post.body}</li>`
+    }
+    output += "</ul>";
+
+    document.querySelector("#response").innerHTML = output;
+
 }
